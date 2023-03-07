a5d8b7eee9a67ec837a9091659a0ac933dc739f2
JarredTD <jarred.desrosiers@yahoo.com>
2023-03-07 16:32:52 -0500

    Missing semicolons on mulitiple lines

--- obj/product.h
+++ obj/product.h
@@ -4,34 +4,34 @@
 #include <string>
 
 class Product {
-    public:
+public:
     
-        // Constructors
-        Product();
-        Product(const std::string& name, const std::string& description, const double& price, const int& quantity, const stdd:string& supplier);
-    
-        // Getters
-        const std::string& getName() const;
-        const std::string& getDescription() const;
-        const std::string& getSupplier() const;
-        int getPrice() const;
-        int getQuantity() const;
+    // Constructors
+    Product();
+    Product(const std::string& name, const std::string& description, const double& price, const int& quantity, const std::string& supplier);
 
-        //Setters
-        void setName(const std::string& name);
-        void setDescription(const std::string& description);
-        void setSupplier(const std::string& supplier);
-        void setPrice(double price)
-        void setQuantity(int quantity)
+    // Getters
+    const std::string& getName() const;
+    const std::string& getDescription() const;
+    const std::string& getSupplier() const;
+    int getPrice() const;
+    int getQuantity() const;
+
+    //Setters
+    void setName(const std::string& name);
+    void setDescription(const std::string& description);
+    void setSupplier(const std::string& supplier);
+    void setPrice(double price);
+    void setQuantity(int quantity);
         
 
-    private:
-        // Data
-        int price_;
-        int quantity_;
-        std::string name_;
-        std::string description_;
-        std::string 
+private:
+    // Data
+    int price_;
+    int quantity_;
+    std::string name_;
+    std::string description_;
+    std::string supplier_;
 };
 
 #endif
