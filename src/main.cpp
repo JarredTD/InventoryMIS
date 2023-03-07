eeb0a4531b74b8b796f3fbee2ddc3639898ed82f
JarredTD <jarred.desrosiers@yahoo.com>
2023-03-07 18:04:33 -0500

    Implement getters

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include "../obj/money.h"
 
 int main(){
-    
+    Money* price_0 = new Money();
+    Money* price = new Money(13.5);
+
+    printf("%i.%i", price_0->getDollar(), price_0->getChange());
+    printf("%i.%i", price_0->getDollar(), price_0->getChange());
+
+    delete price_0;
+    delete price;
     return 0;
 }
